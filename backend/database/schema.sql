361050b1052234a5923e0e1fe35aebee208d6020
Joshua Olsen <joshua.olsen14@gmail.com>
2021-08-02 11:47:00 -0400

    Updated schema.sql to create the potholes table and all fields within.

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -2,8 +2,16 @@ BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS seq_user_id;
+DROP TABLE IF EXISTS potholes;
+DROP SEQUENCE IF EXISTS seq_pothole_id;
 
 CREATE SEQUENCE seq_user_id
+  INCREMENT BY 1
+  NO MAXVALUE
+  NO MINVALUE
+  CACHE 1;
+
+CREATE SEQUENCE seq_pothole_id
   INCREMENT BY 1
   NO MAXVALUE
   NO MINVALUE
@@ -16,6 +24,24 @@ CREATE TABLE users (
 	password_hash varchar(200) NOT NULL,
 	role varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
+);
+
+CREATE TYPE pothole_status AS ENUM ('uninspected', 'under inspection', 'inspected', 'under repair', 'repaired');
+CREATE TYPE pothole_dimensions AS ENUM ('0-1ft', '1-2ft', '2+ft', 'sinkhole');
+
+CREATE TABLE potholes (
+	pothole_id int DEFAULT nextval('seq_pothole_id'::regclass) NOT NULL,
+	submitter_id int,
+	lat varchar(50) NOT NULL,
+	lon varchar(50) NOT NULL,
+	addr varchar(150),
+	neighborhood varchar(150),
+	date_created timestamp NOT NULL,
+	current_status pothole_status,
+	dimensions pothole_dimensions,
+	notes varchar(250),
+	CONSTRAINT FK_pothole_user FOREIGN KEY (submitter_id) REFERENCES users (user_id),
+	CONSTRAINT PK_pothole PRIMARY KEY (pothole_id)
 );
 
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
