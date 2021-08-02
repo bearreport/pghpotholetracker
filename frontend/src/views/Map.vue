e8ea216b4fbe6d3e25c667e58a188031fcde23e7
Josh Bayer <bayerj@gmail.com>
2021-08-02 14:15:39 -0400

    hereMap implemented

---
+++ frontend/src/views/Map.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+      <HereMap :center="center" />
+  </div>  
+</template>
+
+<script>
+import HereMap from '../components/HereMap'
+export default {
+  name: 'app',
+  components: {
+    HereMap
+    // Remove the HelloWorld.vue 
+  },
+  data() {
+return {
+    // we are this as prop to the HereMap component 
+  center:{ 
+    lat: 40.42387869, 
+    lng: -79.9779719
+    }
+}
+  
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
