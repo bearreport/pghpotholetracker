64be40d9fdeda28260144a0519c468bcb2066cd8
Josh Bayer <bayerj@gmail.com>
2021-08-02 20:42:46 -0400

    will you accept this?

--- frontend/src/components/HereMap.vue
+++ frontend/src/components/HereMap.vue
@@ -55,45 +55,6 @@ export default {
       H.ui.UI.createDefault(map, maptypes, H);
       // End rendering the initial map
 
-      let behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
-      console.log("adding draggable marker")
-      this.addDraggableMarker(map, behavior)
-      console.log("marker added")
-    },
-    addDraggableMarker(map, behavior, here){
-      let marker = new here.map.Marker({lat: 40.42387869, lng: -79.9779719});
-      // Ensure that the marker can receive drag events
-      marker.draggable = true;
-      map.addObject(marker);
-      console.log(marker);
-      // disable the default draggability of the underlying map
-      // when starting to drag a marker object:
-      map.addEventListener('dragstart', function(ev) {
-        let target = ev.target;
-        if (target instanceof here.map.Marker) {
-          behavior.disable();
-        }
-      }, false);
-
-
-      // re-enable the default draggability of the underlying map
-      // when dragging has completed
-      map.addEventListener('dragend', function(ev) {
-        let target = ev.target;
-        if (target instanceof here.map.Marker) {
-          behavior.enable();
-        }
-      }, false);
-
-      // Listen to the drag event and move the position of the marker
-      // as necessary
-      map.addEventListener('drag', function(ev) {
-        let target = ev.target,
-            pointer = ev.currentPointer;
-        if (target instanceof here.map.Marker) {
-          target.setPosition(map.screenToGeo(pointer.viewportX, pointer.viewportY));
-        }
-      }, false);
     }
   }
 };
